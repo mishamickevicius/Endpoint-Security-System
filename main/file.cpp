087310005b261a938f1c9a3e675ad683b144abbd
Misha M <misham@Mishas-MacBook-Air.local>
2025-04-23 13:49:48 -0500

    file.cpp changes

---
+++ main/file.cpp
@@ -0,0 +1,14 @@
+/* 
+file.cpp 
+This file will manage file I/O 
+We need a way to save packet data for use such as
+a medium between the C++ code and the ML model/agent
+*/
+
+#include <iostream>
+#include <fstream> // Class for managing file operations
+
+int main() {
+    
+    return 0;
+}
